dd2a34da43b77aaf353d1e975fb5b288682c29da
codestack82 <{Eatsleepcoderepeat}>
2019-06-15 11:13:38 -0500

    s5-test

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import db
 
 
-#User Class
+# User Class
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# Create a flask shell context within the app for the db
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post':Post}
