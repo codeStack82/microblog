49f23132468dc2d0990f5d3399689e6d00123877
codestack82 <{Eatsleepcoderepeat}>
2019-06-16 15:33:33 -0500

    add flask login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # User Class
@@ -13,6 +14,13 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Posts Class
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -21,9 +29,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-
+        
 #Code commentary
 # initialize db using flask-migrate
 ## flask db init
